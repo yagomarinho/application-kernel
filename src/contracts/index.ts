23354e1fd60d64f328267d60fb0d9887a773dc80
Yago Marinho <marinho.rj2@hotmail.com>
2026-01-11 07:34:41 -0300

    contracts

---
+++ src/contracts/index.ts
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2025 Yago Marinho
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+export * from './contracts'
+export * from './pipeline'
+export * from './ports'
+export * from './repository'
+export * from './transactions'
+export * from './use.cases'
